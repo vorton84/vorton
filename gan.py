236e1ec640d0db37c32d8075414d65d527aaacf8
vorton84 <vorton84@gmail.com>
2022-06-17 09:37:44 +0900

    Add files via upload

---
+++ gan.py
@@ -0,0 +1,50 @@
+"""
+Codes from : https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+"""
+
+import torch
+import torch.nn as nn
+
+from typing import TypeVar
+Tensor = TypeVar('torch.tensor')
+
+
+nc = 3
+nz = 100
+ngf = 64
+
+
+class GAN(nn.Module):
+    def __init__(self):
+        super(GAN, self).__init__()
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+        
+    def decode(self, z: Tensor) -> Tensor:
+        """
+        Maps the given latent codes
+        onto the image space.
+        :param z: (Tensor) [B x D]
+        :return: (Tensor) [B x C x H x W]
+        """
+        return self.main(z[..., None, None])
